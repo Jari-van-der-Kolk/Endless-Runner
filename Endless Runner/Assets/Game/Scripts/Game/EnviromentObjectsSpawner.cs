f3810b508334d5b5a3447046f531572e9607177a
Jari van der Kolk <533467@student.glu.nl>
2021-02-18 16:20:26 +0100

    added stuff
    - added the OutOffBounds
    - added the object spawner

--- Endless Runner/Assets/Game/Scripts/Game/EnviromentObjectsSpawner.cs
+++ Endless Runner/Assets/Game/Scripts/Game/EnviromentObjectsSpawner.cs
@@ -4,7 +4,31 @@ using UnityEngine;
 
 public class EnviromentObjectsSpawner : MonoBehaviour
 {
-    public List<GameObject> tinyObjects;
-    public List<GameObject> platforms;
+    public float spawnIndex;
+    public FloatVariable spawnChance;
+    public Placebels[] placebels;
+
+    private void Start()
+    {
+        foreach (Placebels p in placebels)
+        {
+            int randomNumber = Random.Range(0,Mathf.RoundToInt(spawnChance.Value));
+            if (randomNumber <= spawnIndex)
+            {
+                Instantiate(ReturnGameobjectFromArray(p.Objects), p.Location.transform.position, Quaternion.identity);
+            }
+            
+        }
+    }
+    private GameObject ReturnGameobjectFromArray(GameObject[] objects) => objects[Random.Range(0, objects.Length)];
+}
+
+[System.Serializable]
+public class Placebels
+{
+    public string name;
+    public Transform Location;
+    public GameObject[] Objects;
+
 }
 
